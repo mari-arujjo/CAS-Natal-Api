deadde9b4b4c1aa17de8a9f0e1ad8db1903b5901
Mariana <120474044+mari-arujjo@users.noreply.github.com>
2025-09-14 15:07:51 -0300

    configurações do JWT

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,9 @@
 using api;
+using api.AppUserIdentity.Model;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,6 +11,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Configura o Identity
+builder.Services.AddIdentity<AppUserModel, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = false; // numeros obrigatorios na senha
+    options.Password.RequireLowercase = false; // letras minusculas obrigatorias na senha
+    options.Password.RequireUppercase = false; // letras maiusculas obrigatorias na senha
+    options.Password.RequireNonAlphanumeric = false; // caracteres especiais obrigatorios na senha
+    options.Password.RequiredLength = 5; // tamanho minimo da senha
+}).AddEntityFrameworkStores<ApplicationDbContext>();
+
+//Configura o JWT
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme =
+    options.DefaultChallengeScheme =
+    options.DefaultForbidScheme =
+    options.DefaultScheme =
+    options.DefaultSignInScheme =
+    options.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options => {
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true, // valida a chave de assinatura do token 
+        ValidIssuer = builder.Configuration["JWT:Issuer"], // emissor do token
+        ValidateAudience = true, // valida o publico do token
+        ValidAudience = builder.Configuration["JWT:Audience"], // audiencia do token
+        ValidateIssuerSigningKey = true, // valida a chave de assinatura do token
+        IssuerSigningKey = new SymmetricSecurityKey(
+            System.Text.Encoding.UTF8.GetBytes(
+                builder.Configuration["JWT:SigningKey"] // chave de assinatura do token
+            )
+        )
+    };
 });
 
 
@@ -22,5 +61,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseAuthentication(); // Habilita a autenticação
+app.UseAuthorization(); // Habilita a autorização
 
 app.Run();
