6d56c26b8649788a91ba2e48c87a14d625c68e62
Mariana Araújo <mariana.estudo9889@gmail.com>
2025-09-29 12:53:51 -0300

    feat: implementando mapper do course

---
+++ api/Courses/Dtos/CourseMapper.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System;
+
+namespace api.Courses.Dtos
+{
+    // Nessa classe, iremos criar um método de extensão.
+    // Permite adicionar um novo método a uma classe existente sem precisar modificar essa classe diretamente ou criar uma subclasse.
+    public static class CourseMapper
+    {
+        public static CourseDto ConvertToCourseDto(this Course c)
+        {
+            return new CourseDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Abbreviation = c.Abbreviation,
+                Description = c.Description,
+                Photo = c.Photo
+            };
+        }
+
+        public static Course CreateNewCourseDto(this CourseDto c)
+        {
+            return new Course
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Abbreviation = c.Abbreviation,
+                Description = c.Description,
+                Photo = c.Photo
+            };
+        }
+
+    }
+}
